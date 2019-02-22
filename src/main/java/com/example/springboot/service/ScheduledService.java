c3aec6da2be67fb74cc56acfbcf84947cf89748b
miaozr007 <1611796513@qq.com>
2019-02-22 16:26:14 +0800

    task

---
+++ src/main/java/com/example/springboot/service/ScheduledService.java
@@ -0,0 +1,29 @@
+package com.example.springboot.service;
+
+import com.example.springboot.RedisLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledService {
+    private final Logger logger = LoggerFactory.getLogger(ScheduledService.class);
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Scheduled(fixedRate = 5000)
+    // lockPredix:项目前缀  完整的key:lockPrefix+lockKey两者都必填 timeOut单位为秒
+    @RedisLock(lockPrefix ="springboot",lockKey = "testkey",timeOut = 5)
+    public void scheduledTest1(){
+            logger.info("定时任务1");
+    }
+
+    @Scheduled(fixedRate = 5000)
+    @RedisLock(lockPrefix ="springboot",lockKey = "testkey",timeOut = 5)
+    public void scheduledTest2(){
+            logger.info("定时任务2");
+    }
+}
---
+++ src/main/java/com/example/springboot/utils/Tools.java
@@ -0,0 +1,49 @@
+package com.example.springboot.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class Tools {
+    public static String exePython(String filePath, Object... args) {
+        String result = "";
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {//判断文件目录的存在
+                File fileDir = new File(file.getParent());
+                fileDir.mkdirs();
+            }
+
+            StringBuffer parameter = new StringBuffer("python ");
+            parameter.append(filePath);
+            parameter.append(" ");
+            if (args instanceof Object[]) {
+                for (Object arg : args) {
+                    parameter.append(arg);
+                    parameter.append(" ");
+                }
+            }
+
+            Process pr = Runtime.getRuntime().exec(parameter.toString());
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    pr.getInputStream(), Charset.forName("UTF-8")));
+            String line;
+            StringBuffer sbf = new StringBuffer();
+            while ((line = in.readLine()) != null) {
+                sbf.append(line);
+            }
+            result = sbf.toString();
+        } catch (IOException e) {
+            System.out.println("执行python文件失败。");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String filePath = "/Users/xiaoshan/wdzj_wechat.py";
+        String result = Tools.exePython(filePath,"刘雄雄","你好，我是机器人");
+        System.out.println(result);
+    }
+}
